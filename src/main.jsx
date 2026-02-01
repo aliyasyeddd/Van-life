6f9c61da867e2d756a98f3ba3ee0c2a22b2dcaed
aliya mahaboob <mahaboobaliya02@gmail.com>
2026-02-01 12:24:00 +0530

    Add Mirage mock API and Vans pages
    Introduce a MirageJS mock server and basic vans UI. Adds src/server.js to provide /api/vans and /api/vans/:id endpoints, new Vans and VanDetail pages (src/pages/Vans.jsx, src/pages/VanDetail.jsx), and wires them into routing in src/main.jsx. Also includes styling for the vans list in src/index.css and adds miragejs to package.json as a dev dependency.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import About from "./pages/About.jsx";
 import Home from "./pages/Home.jsx";
+import Vans from "./pages/Vans.jsx";
+import VanDetail from "./pages/VanDetail.jsx";
 import "./index.css";
+import "./server";
 
 function App() {
   return (
@@ -12,11 +15,14 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+           <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
