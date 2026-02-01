e9361ede771c0c0f74ae1a521405319073dd4560
aliya mahaboob <mahaboobaliya02@gmail.com>
2026-02-01 17:57:21 +0530

    Extract Header/Layout and implement VanDetail
    Add a Header component and Layout wrapper (with Outlet) and update routing to nest pages under Layout; removes inline header markup from main.jsx and cleans up unused imports. Implement VanDetail page to fetch and render a single van's data from /api/vans/:id and add related CSS styles for the van detail view and buttons to src/index.css. These changes DRY the header across pages and provide a full van detail UI with loading state.

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About.jsx";
 import Home from "./pages/Home.jsx";
 import Vans from "./pages/Vans.jsx";
 import VanDetail from "./pages/VanDetail.jsx";
+import Layout from "./components/Layout.jsx";
 import "./index.css";
 import "./server";
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-           <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route  element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
