bc7031508a3176881e322c8dfc8d61838a82b06d
aliya mahaboob <mahaboobaliya02@gmail.com>
2026-02-03 18:33:49 +0530

    implementing footer , host routes

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import Dashboard from "./pages/Host/Dashboard.jsx";
 import Income from "./pages/Host/Income.jsx";
 import Reviews from "./pages/Host/Reviews.jsx";
 import Layout from "./components/Layout.jsx";
+import HostLayout from "./components/HostLayout.jsx";
 import "./index.css";
 
 import "./server";
@@ -17,14 +18,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />}>
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+            <Route path="vans" element={<Vans />} />
+            <Route path="vans/:id" element={<VanDetail />} />
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
