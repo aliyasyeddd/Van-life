6e1bf76123da5eba92de5b0680966c60969c87c6
aliya mahaboob <mahaboobaliya02@gmail.com>
2026-02-06 05:23:31 +0530

    login credentials

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,31 @@
-import React from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { loginUser } from "../api";
 
 export default function Login() {
-  const [loginFormData, setLoginFormData] = React.useState({
+  const [loginFormData, setLoginFormData] = useState({
     email: "",
     password: "",
   });
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null);
+
   const location = useLocation();
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(loginFormData);
+    setStatus("submitting");
+    loginUser(loginFormData)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(err);
+      })
+      .finally(() => {
+        setStatus("idle");
+      });
   }
 
   function handleChange(e) {
@@ -21,13 +36,13 @@ export default function Login() {
     }));
   }
 
-
   return (
     <div className="login-container">
       {location.state?.message && (
         <h3 className="login-first">{location.state.message}</h3>
       )}
       <h1>Sign in to your account</h1>
+      {error?.message && <h3 className="login-first">{error.message}</h3>}
       <form onSubmit={handleSubmit} className="login-form">
         <input
           name="email"
@@ -43,7 +58,9 @@ export default function Login() {
           placeholder="Password"
           value={loginFormData.password}
         />
-        <button>Log in</button>
+        <button disabled={status === "submitting"}>
+          {status === "submitting" ? "Logging in..." : "Log in"}
+        </button>
       </form>
     </div>
   );
--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -1,27 +1,46 @@
-import React from "react";
 import { useEffect, useState } from "react";
 import { useParams, Link, useLocation } from "react-router-dom";
+import { getVans } from "../../api";
 
 export default function VanDetail() {
   const [van, setVan] = useState(null);
-  const params = useParams();
-   const location = useLocation()
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { id } = useParams();
+  const location = useLocation();
 
   useEffect(() => {
-    fetch(`/api/vans/${params.id}`)
-      .then((res) => res.json())
-      .then((data) => setVan(data.vans));
-  }, [params.id]);
+    async function loadVans() {
+      setLoading(true);
+      try {
+        const data = await getVans(id);
+        setVan(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadVans();
+  }, [id]);
 
-  const search = location.state?.search || ""
-  const type = location.state?.type || "all"
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>There was an error: {error.message}</h1>;
+  }
+
+  const search = location.state?.search || "";
+  const type = location.state?.type || "all";
 
   return (
     <div className="van-detail-container">
       <Link to={`..${search}`} relative="path" className="back-button">
         &larr; <span>Back to {type} vans</span>
       </Link>
-      {van ? (
+      {van && (
         <div className="van-detail">
           <img src={van.imageUrl} />
           <i className={`van-type ${van.type} selected`}>{van.type}</i>
@@ -32,8 +51,6 @@ export default function VanDetail() {
           <p>{van.description}</p>
           <button className="link-button">Rent this van</button>
         </div>
-      ) : (
-        <h2>Loading...</h2>
       )}
     </div>
   );
