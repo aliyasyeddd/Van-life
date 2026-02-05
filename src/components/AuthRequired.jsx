5fa5961c2ed1776e0a56a2fb4bc05244b4734b1f
aliya mahaboob <mahaboobaliya02@gmail.com>
2026-02-05 22:03:03 +0530

    Navigation state

--- src/components/AuthRequired.jsx
+++ src/components/AuthRequired.jsx
@@ -1,11 +1,13 @@
-import React from "react"
-import { Outlet, Navigate } from "react-router-dom"
+import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 
 export default function AuthRequired() {
-    const authenticated = false
-    
-    if (!authenticated) {
-        return <Navigate to="/login" />
-    }
-    return <Outlet />
+  const authenticated = false;
+
+  if (!authenticated) {
+    return (
+      <Navigate to="/login" state={{ message: "You must log in first" }} />
+    );
+  }
+  return <Outlet />;
 }
